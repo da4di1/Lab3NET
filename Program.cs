351ec0ac700a56832fcd6c4afc16fc8b5638c649
da4di1 <71738970+da4di1@users.noreply.github.com>
2022-06-14 18:14:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputerStore;
+
+namespace Lab3NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembler assembler = new Assembler();
+            SystemUnitBuilder builder = new IntelSysUnitBuilder();
+
+            SystemUnit intelSysUnit = assembler.Assemble(builder);
+            Console.WriteLine("Intel System Unit:\n" + intelSysUnit);
+
+            Console.WriteLine("\n");
+
+            builder = new AMDSysUnitBuilder();
+            SystemUnit amdSysUnit = assembler.Assemble(builder);
+            Console.WriteLine("AMD System Unit:\n" + amdSysUnit);
+        }
+    }
+}
