658fe2108a14de14cbc1ab8edb0e4c9582a3e807
da4di1 <71738970+da4di1@users.noreply.github.com>
2022-06-14 18:09:48 +0300

    Add files via upload

---
+++ Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerStore
+{
+    public class Box
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ SystemUnitBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerStore
+{
+    public abstract class SystemUnitBuilder
+    {
+        public SystemUnit SystemUnit { get; set; }
+
+
+        public void CreateSysUnit()
+        {
+            SystemUnit = new SystemUnit();
+        }
+
+        public abstract void SetBox();
+        public abstract void SetProcessor();
+        public abstract void SetMainBoard();
+        public abstract void SetHdd();
+        public abstract void SetMemory();
+    }
+}
